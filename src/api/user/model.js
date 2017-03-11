27d5e58fee50981f40603021308d1b3a9a562498
humblehound <lukmyslinski@gmail.com>
2017-03-11 19:04:58 +0000

    Updated bcrypt

--- src/api/user/model.js
+++ src/api/user/model.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import randtoken from 'rand-token'
 import mongoose, { Schema } from 'mongoose'
 import mongooseKeywords from 'mongoose-keywords'
